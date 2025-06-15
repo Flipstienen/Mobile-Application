1a590e8987065cd9181e8e1c1b6faffe5837947d
Flipstienen <flipstienen1@gmail.com>
2025-06-15 13:26:51 +0200

    changes from other group member with .net 8
    other user made changes and i have put this in our .net 8 version.

---
+++ MobileApplication.Maui/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using MobileApplication.Core.Model;
+using MobileApplication.Core.Helpers;
+namespace MobileApplication.Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+	
+	
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
