f1ab220e521c1d7217e9995b6da19c11982b535e
ThatDevFoxy <ThatDevFoxy@gmail.com>
2025-06-17 09:50:55 +0200

    Start on the new mainpage
    - Added usr icon
    - Got buttons to work
    - Added extra style
    - why user icon no work
    - why button no outline middle :(

--- MobileApplication.Maui/MainPage.xaml.cs
+++ MobileApplication.Maui/MainPage.xaml.cs
@@ -1,8 +1,4 @@
-﻿using MobileApplication.Core.Helpers;
-using MobileApplication.Core.Model;
-using System;
-using System.Linq;
-using System.Text.Json;
+﻿using Microsoft.Maui.Controls;
 
 namespace MobileApplication.Maui;
 
@@ -13,39 +9,23 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
-    protected override async void OnAppearing()
+    private void OnCurrentOrderClicked(object sender, EventArgs e)
     {
-        base.OnAppearing();
-        await LoadOrderAsync();
+        DisplayAlert("Current Order", "Navigating to Current Order", "OK");
     }
 
-    private async Task LoadOrderAsync()
+    private void OnScheduleClicked(object sender, EventArgs e)
     {
-        try
-        {
-            var shallowOrders = await ApiHelper.Instance.GetAsync<List<Order>>("/api/Order");
-            var fullOrders = new List<Order>();
+        DisplayAlert("Schedule", "Navigating to Schedule", "OK");
+    }
 
-            foreach (var o in shallowOrders)
-            {
-                var fullOrder = await ApiHelper.Instance.GetAsync<Order>($"/api/Order/{o.Id}");
-                fullOrders.Add(fullOrder);
-            }
+    private void OnTrackingClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Tracking", "Navigating to Tracking", "OK");
+    }
 
-            OrderCollectionView.ItemsSource = fullOrders.Select(o => new
-            {
-                o.Id,
-                o.OrderDate,
-                CustomerDisplay = $"Customer: {o.Customer?.Name ?? "N/A"}, Address: {o.Customer?.Address ?? "N/A"}",
-                ProductsDisplay = "Products: " + string.Join(", ", o.Products?.Select(p => p.Name) ?? new List<string> { "N/A" }),
-                DeliveryStateDisplay = o.DeliveryStates != null && o.DeliveryStates.Any()
-                    ? $"Latest Delivery State: {o.DeliveryStates.OrderByDescending(ds => ds.DateTime).First().State} @ {o.DeliveryStates.OrderByDescending(ds => ds.DateTime).First().DateTime:yyyy-MM-dd HH:mm}"
-                    : "No delivery states"
-            }).ToList();
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlert("Error", ex.Message, "OK");
-        }
+    private void OnWeatherClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Weather", "Navigating to Weather", "OK");
     }
 }
