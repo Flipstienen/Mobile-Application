71feaefcb71f949895a2fb77e87aae788331d258
Flipstienen <flipstienen1@gmail.com>
2025-06-18 13:18:26 +0200

    I have fixed the post i just need to display it right on the front end

--- MobileApplication.Maui/Pages/SchedulePage.xaml.cs
+++ MobileApplication.Maui/Pages/SchedulePage.xaml.cs
@@ -1,6 +1,7 @@
 using System.Runtime.InteropServices.Marshalling;
 using MobileApplication.Core.Helpers;
 using MobileApplication.Core.Model;
+using MobileApplication.Maui.ViewModel;
 
 namespace MobileApplication.Maui.Pages;
 
@@ -14,99 +15,6 @@ public partial class SchedulePage : ContentPage
     protected override async void OnAppearing()
     {
         base.OnAppearing();
-        await LoadOrder();
-    }
-    private async Task LoadOrder()
-    {
-        string apiKey = EnvHelper.Instance.GetEnvironmentVariable("API_KEY", "");
-        var DeliveryServices = await ApiHelper.Instance.GetAsync<DeliveryService>($"/api/DeliveryServices/{apiKey}");
-
-        if (DeliveryServices == null)
-        {
-            await DisplayAlert("Error", "Delivery service not found!", "OK");
-            return;
-        }
-
-        try
-        {
-            await CreateNewOrderAsync();
-            var orderlist = await ApiHelper.Instance.GetAsync<List<Order>>($"/api/Order?deliveryServiceId={DeliveryServices.id}");
-            var order = await ApiHelper.Instance.GetAsync<Order>($"/api/Order/1");
-
-            if (order != null)
-            {
-                OrderIdLabel.Text = order.Id.ToString();
-                CustomerNameLabel.Text = order.Customer?.Name ?? "Unknown";
-                CustomerAdressLabel.Text = order.Customer?.Address ?? "Unknown";
-                ProductsLabel.Text = (order.Products != null && order.Products.Any())
-                    ? string.Join(", ", order.Products.Select(p => p.Name))
-                    : "No products";
-                OrderDateLabel.Text = order.OrderDate.ToString("g");
-                if (order.DeliveryStates != null && order.DeliveryStates.Any())
-                {
-                    DeliveryServiceLabel.Text = order.DeliveryStates.LastOrDefault().Id.ToString();
-                }
-                else
-                {
-                    DeliveryServiceLabel.Text = "No delivery states found";
-                }
-            }
-            else
-            {
-                Console.WriteLine("Order details not found.");
-                await DisplayAlert("Warning", "Order details not found.", "OK");
-                OrderIdLabel.Text = "Not Found";
-                CustomerNameLabel.Text = "Not Found";
-                CustomerAdressLabel.Text = "Not Found";
-                ProductsLabel.Text = "Not Found";
-                OrderDateLabel.Text = "Not Found";
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"API Error: {ex.Message}");
-            await DisplayAlert("Error", $"API Error: {ex.Message}", "OK");
-        }
-    }
-
-    private async Task CreateNewOrderAsync()
-    {
-        try
-        {
-            string apiKey = EnvHelper.Instance.GetEnvironmentVariable("API_KEY", "");
-            var DeliveryServices = await ApiHelper.Instance.GetAsync<DeliveryService>($"/api/DeliveryServices/{apiKey}");
-            var newOrder = new Order
-            {
-                OrderDate = DateTime.UtcNow,
-                CustomerId = 1,
-                Products = new List<Product>
-                {
-                    new Product
-                            {
-                                Id = 1,
-                                Name = "Nebuchadnezzar",
-                                Description = "Het schip waarop Neo voor het eerst de echte wereld leert kennen",
-                                Price = 10000.0,
-                            }
-                },
-                DeliveryStates = new List<DeliveryState>
-                {
-                    new DeliveryState
-                    {
-                        State = 1,
-                        DateTime = DateTime.UtcNow,
-                        DeliveryServiceId = DeliveryServices.id
-                    }
-                }
-            };
-            var result = await ApiHelper.Instance.PostAsync<Order>("/api/Order", newOrder);
-            Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(result));
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlert("Error", ex.Message, "OK");
-        }
-
+        await SchedulePageViewModel.LoadOrder();
     }
 }
-
---
+++ MobileApplication.Maui/ViewModel/SchedulePageViewModel.cs
@@ -0,0 +1,87 @@
+﻿using MobileApplication.Maui.Pages;
+using MobileApplication.Maui;
+using MobileApplication.Core.Helpers;
+using MobileApplication.Core.Model;
+
+namespace MobileApplication.Maui.ViewModel
+{
+    public class SchedulePageViewModel
+    {
+        public async Task LoadOrder()
+        {
+            string apiKey = EnvHelper.Instance.GetEnvironmentVariable("API_KEY", "");
+            var DeliveryServices = await ApiHelper.Instance.GetAsync<DeliveryService>($"/api/DeliveryServices/{apiKey}");
+
+            if (DeliveryServices == null)
+            {
+                Console.WriteLine("Error", "Delivery service not found!", "OK");
+                return;
+            }
+
+            try
+            {
+                await CreateNewOrderAsync();
+                var orderlist = await ApiHelper.Instance.GetAsync<List<Order>>($"/api/Order?deliveryServiceId={DeliveryServices.id}");
+
+                if (order != null)
+                {
+                    OrderIdLabel.Text = order.Id.ToString();
+                    CustomerNameLabel.Text = order.Customer?.Name ?? "Unknown";
+                    CustomerAdressLabel.Text = order.Customer?.Address ?? "Unknown";
+                    OrderDateLabel.Text = order.OrderDate.ToString("g");
+                    if (order.DeliveryStates != null && order.DeliveryStates.Any())
+                    {
+                        DeliveryServiceLabel.Text = order.DeliveryStates.LastOrDefault().State.ToString();
+                    }
+                    else
+                    {
+                        DeliveryServiceLabel.Text = "No delivery states found";
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Order details not found.");
+                    await DisplayAlert("Warning", "Order details not found.", "OK");
+                    OrderIdLabel.Text = "Not Found";
+                    CustomerNameLabel.Text = "Not Found";
+                    CustomerAdressLabel.Text = "Not Found";
+                    OrderDateLabel.Text = "Not Found";
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"API Error: {ex.Message}");
+                await DisplayAlert("Error", $"API Error: {ex.Message}", "OK");
+            }
+        }
+
+        private async Task CreateNewOrderAsync()
+        {
+            try
+            {
+                var lastDate = Preferences.Get("LastUpdateDate", "");
+                if (!DateTime.TryParse(lastDate, out DateTime parsedLastDate) || parsedLastDate.Date < DateTime.Today)
+                {
+                    for (int i = 0; i < 15; i++)
+                    {
+                        string apiKey = EnvHelper.Instance.GetEnvironmentVariable("API_KEY", "");
+                        var DeliveryServices = await ApiHelper.Instance.GetAsync<DeliveryService>($"/api/DeliveryServices/{apiKey}");
+                        var newOrder = new Order
+                        {
+                            OrderDate = DateTime.UtcNow,
+                            CustomerId = 1,
+                        };
+
+                        await ApiHelper.Instance.CreateOrderAsync<Order>(newOrder);
+                    }
+                    Preferences.Set("LastUpdateDate", DateTime.Today.ToString("yyyy-MM-dd"));
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+    }
+}
+
