5c5dd4b8cc88ea054530b9a2f0b7f32b7630d0fd
Flipstienen <flipstienen1@gmail.com>
2025-06-21 16:02:02 +0200

    possibility to include notification

--- MobileApplication.Maui/MauiProgram.cs
+++ MobileApplication.Maui/MauiProgram.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using Shiny;
+using Shiny.Notifications;
+
 namespace MobileApplication.Maui;
 
 public static class MauiProgram
@@ -6,19 +9,32 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
+
         DotNetEnv.Env.Load();
+
         builder
             .UseMauiApp<App>()
+            .UseShiny()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+        builder.Services.AddNotifications<SampleNotificationDelegate>();
 #if DEBUG
         builder.Logging.AddDebug();
+
 #endif
 
         return builder.Build();
+    }
+}
+
+public class SampleNotificationDelegate : INotificationDelegate
+{
+    public Task OnEntry(NotificationResponse response)
+    {
+        return Task.CompletedTask;
     }
 }
